a571ffbb1bb402f4bf8df62434c33ce9e8cd6729
Jeff Eldridge <eldridge.jeff@gmail.com>
2014-08-19 11:46:40 +0100

    Refactored titlecase directive to a service

--- app/scripts/user-app.js
+++ app/scripts/user-app.js
@@ -51,35 +51,11 @@ userApp.filter ('fromNow', function () {
     }
 });
 
-userApp.filter ('titlecase', function () {
+userApp.filter ('titlecase', ['ZenString', function (ZenString) {
    return function (v) {
-
-       var i, j, str, lowers, uppers;
-       str = v.replace(/\b[\w-\']+/g , function(txt) {
-       //str = v.replace(/\b\w+(?:'s)*/g, function(txt) {
-           return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-       });
-
-       // Certain minor words should be left lowercase unless
-       // they are the first or last words in the string
-       lowers = ['A', '&Amp;', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At',
-           'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
-       for (i = 0, j = lowers.length; i < j; i++)
-           str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'),
-               function(txt) {
-                   return txt.toLowerCase();
-               });
-
-       // Certain words such as initialisms or acronyms should be left uppercase
-       uppers = ['Id', 'Tv'];
-       for (i = 0, j = uppers.length; i < j; i++)
-           str = str.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'),
-               uppers[i].toUpperCase());
-
-       return str;
-
+       return ZenString.toTitleCase (v);
    }
-});
+}]);
 
 userApp.service ('Members', function () {
 
