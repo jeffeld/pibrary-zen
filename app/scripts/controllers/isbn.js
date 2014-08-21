5d117721938c8b7c8706377aff8282d0c0f22e8a
Jeff Eldridge <eldridge.jeff@gmail.com>
2014-08-21 15:12:35 +0100

    Full text searching - first cut

--- app/scripts/controllers/isbn.js
+++ app/scripts/controllers/isbn.js
@@ -2,22 +2,24 @@ userControllers.controller('ISBNController', ['$scope', '$sce', 'Codes', 'ISBN',
     function ($scope, $sce, Codes, ISBN, Links) {
 
         // $scope.Details = ___isbnDetails;
-        var i;
+        var i,j;
 
         ISBN.get ({isbn : ___isbn13}).
             $promise.then(function (result) {
 
+                j = result;
                 i = result.data[0];
 
                 $scope.Details = {
-                    title : i.title || 'Unknown',
-                    author : i.author_data[0].name || 'Unknown',
+                    title : j.search_title || 'Unknown',
+                    author : j.search_author || '',
                     publisher : (i.publisher_name || i.publisher_text) || 'Unknown Publisher',
                     edition :  i.edition_info || '',
                     physical : i.physical_description_text || '',
                     summary : (i.summary || i.notes) || '',
                     isbn10: i.isbn10 || 'n/a',
-                    isbn13: i.isbn13 || 'n/a'
+                    isbn13: i.isbn13 || 'n/a',
+                    stock_codes : j.stock_codes
                 };
 
 
